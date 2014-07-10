b1d3bb4306c3d250b2870b350000d64755eccf29
Edson Carvalho <edsonlcj@gmail.com>
2014-07-10 16:35:50 -0300

    track event ga do botão do post de inscrição para o pagseguro

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,20 @@ $('document').ready(function() {
 		ga('send', 'event', 'nav', 'enter', navLabel);
 	});
 	
+	$('#formInscricao').on('submit', function(e) {
+		e.preventDefault();
+		ga('send', 'event', {
+			'eventCategory' : 'form',
+			'eventAction'   : 'submit',
+			'eventLabel'    : 'inscricao',
+			'hitCallback'   : function () {
+				$('#formInscricao').off('submit');
+				$('#formInscricao')[0].submit();
+			}
+		});
+		return false;
+	});
+	
 	/*$('ul.nav li a').on('click', function(e) {
 		var index = $('ul.nav li a').index(this),
 			diffHeader = 50, //$('.navbar-header').height(),
